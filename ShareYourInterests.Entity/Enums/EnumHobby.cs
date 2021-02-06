878573a5058169a3cc6487dbfb38b55e74b4f719
mayiqun <mayiqun@lonwin.org>
2021-02-06 14:34:14 +0800

    添加 爱好 枚举

---
+++ ShareYourInterests.Entity/Enums/EnumHobby.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShareYourInterests.Entity.Enums
+{
+   public enum EnumHobby
+    {
+        Sport,
+        Game,
+        Food
+    }
+}
