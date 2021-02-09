1a524bd5e9a3ea8e9714e4bd4da871f1e4e1c888
Maxwell <1007499575@qq.com>
2021-02-09 10:16:33 +0800

    加入BundleConfig

---
+++ ShareYourInterests/wwwroot/js/userJs/Login.js
@@ -0,0 +1,18 @@
+﻿new Vue({
+    el: '#app',
+    vuetify: new Vuetify(),
+    data() {
+        return {
+            show1: false,
+            show2: true,
+            show3: false,
+            show4: false,
+            password: 'Password',
+            rules: {
+                required: value => !!value || 'Required.',
+                min: v => v.length >= 8 || 'Min 8 characters',
+                emailMatch: () => (`The email and password you entered don't match`),
+            },
+        }
+    },
+})
