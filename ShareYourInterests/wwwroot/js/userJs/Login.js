418eb214557be9cc5ed884a2788349132be25c55
Maxwell <1007499575@qq.com>
2021-02-10 15:33:53 +0800

    add button style and color

--- ShareYourInterests/wwwroot/js/userJs/Login.js
+++ ShareYourInterests/wwwroot/js/userJs/Login.js
@@ -3,15 +3,12 @@
     vuetify: new Vuetify(),
     data() {
         return {
-            show1: false,
-            show2: true,
-            show3: false,
-            show4: false,
+            showPassword: false,
+            userName:'UserName',
             password: 'Password',
             rules: {
                 required: value => !!value || 'Required.',
-                min: v => v.length >= 8 || 'Min 8 characters',
-                emailMatch: () => (`The email and password you entered don't match`),
+                min: v => v.length >= 8 || 'Min 8 characters'
             },
         }
     },
