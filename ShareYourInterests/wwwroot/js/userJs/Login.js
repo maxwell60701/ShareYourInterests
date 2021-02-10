68ac2693323c31eb47f5d4d08bd801031f1d20e9
Maxwell <1007499575@qq.com>
2021-02-10 17:35:30 +0800

    暂时保存

--- ShareYourInterests/wwwroot/js/userJs/Login.js
+++ ShareYourInterests/wwwroot/js/userJs/Login.js
@@ -4,8 +4,8 @@
     data() {
         return {
             showPassword: false,
-            userName:'UserName',
-            password: 'Password',
+            userAccount:'',
+            userPassword: '',
             rules: {
                 required: value => !!value || 'Required.',
                 min: v => v.length >= 8 || 'Min 8 characters'
