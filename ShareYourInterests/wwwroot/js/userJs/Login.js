6272367fac609bb6c058ebb55b0f10fb12631750
mayiqun <mayiqun@lonwin.org>
2021-02-25 11:28:02 +0800

    add alert

--- ShareYourInterests/wwwroot/js/userJs/Login.js
+++ ShareYourInterests/wwwroot/js/userJs/Login.js
@@ -1,11 +1,12 @@
-﻿new Vue({
+﻿var vm = new Vue({
     el: '#app',
     vuetify: new Vuetify(),
     data() {
         return {
             showPassword: false,
-            userAccount:'',
+            userAccount: '',
             userPassword: '',
+            showAlert: false,
             rules: {
                 required: value => !!value || 'Required.',
                 min: v => v.length >= 8 || 'Min 8 characters'
@@ -15,18 +16,23 @@
     methods:
     {
         Login() {
-            this.$http.post('/Login/UserLogin', { UserAccount: this.userAccount, UserPassword: this.userPassword }).then(
-                function (result) {
-                    if (result.body.code == 200) {
-                        window.location.href = "Home/Index";
-                    }
-                });
+            this.$http.post('/Login/UserLogin', { UserAccount: this.userAccount, UserPassword: this.userPassword })
+                .then(
+                    function (result) {
+                        if (result.body.code == 200) {
+                            window.location.href = "Home/Index";
+                        } else {
+                            this.showAlert = true;
+                            setTimeout(function () { vm.showAlert = false }, 3000);
+                        }
+                    });
         }
+
     },
     computed:
     {
         isButtonDisabled() {
-            return this.userPassword.length < 8 || this.userAccount.length===0;
+            return this.userPassword.length < 8 || this.userAccount.length === 0;
         }
     }
-})
+});
